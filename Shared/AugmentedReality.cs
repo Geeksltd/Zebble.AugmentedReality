7141be1020b230fd0fd814cea17a3b4272af535d
Paymon <paymon@geeks.ltd.uk>
2017-07-28 18:28:33 +0100

    Clean up and references

--- Shared/AugmentedReality.cs
+++ Shared/AugmentedReality.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    using Framework;
     using Zebble.Services;
 
     public class AugmentedReality : Canvas
